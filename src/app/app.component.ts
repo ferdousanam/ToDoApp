ad699e1166bc74cd82b5cac934662e58a4e0b475
Ferdous Anam <ferdous.anam@gmail.com>
2019-09-28 12:38:45 +0600

    Simple To Do Task List Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ToDoTask';
+  appTitle = 'ToDo | ToDoTask';
+  title = 'ToDo Task List';
+  TaskList = [];
+  task = '';
+  countTask = 0;
+  public constructor(private titleService: Title) {
+    this.setTitle(this.appTitle);
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+
+  addTask() {
+    if (this.task != '') {
+      this.TaskList.push(this.task);
+      this.countTask = this.TaskList.length;
+      this.task = '';
+    }
+  }
+
+  removeTask(index) {
+    this.TaskList.splice(index, 1);
+    this.countTask = this.TaskList.length;
+  }
 }
